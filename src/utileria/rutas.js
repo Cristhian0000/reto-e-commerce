1f0f76b08f6edb9d48ded844523073266fcc84b8
Cristhian <farfan463@gmail.com>
2022-09-06 22:14:40 -0500

    change categories endpoint

--- src/utileria/rutas.js
+++ src/utileria/rutas.js
@@ -1,6 +1,6 @@
 const rutas = {
   ALL_PRODUCTS: "http://localhost:3000/products",
-  PRODUCTS_FOR_CATEGORY: "https://fakestoreapi.com/products/category",
+  PRODUCTS_FOR_CATEGORY: "http://localhost:3000/products/categories",
   AUTHENTICATION: "http://localhost:3000/auth",
 };
 export default rutas;
